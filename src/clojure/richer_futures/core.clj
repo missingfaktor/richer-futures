12d9ea235c26beab96d86e38650a1750f7e477d0
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2013-12-01 14:20:57 +0530

    Rahul - Added a blocking await. Also fixed a subtle issue with future macro.

--- src/clojure/richer_futures/core.clj
+++ src/clojure/richer_futures/core.clj
@@ -1,14 +1,22 @@
 (ns richer-futures.core
-  (:import [scala.concurrent ExecutionContext$Implicits$ Future$])
+  (:import [scala.concurrent ExecutionContext$Implicits$ Future Future$ Await$]
+           [clojure.lang IDeref]
+           [scala.concurrent.duration Duration$])
   (:use [richer-futures.interop])
   (:refer-clojure :exclude [future]))
 
 (def ^{:dynamic true} *execution-context*
   (.global ExecutionContext$Implicits$/MODULE$))
 
+(def ^{:dynamic true} *max-await-duration*
+  (.Inf Duration$/MODULE$))
+
 (defn future-fn [fun]
   (.apply Future$/MODULE$ (create-scala-function fun 0) *execution-context*))
 
-(defmacro future [& code]
+(defmacro future [code]
   `(future-fn (fn [] ~code)))
+
+(defn block-and-await [fut]
+  (.result Await$/MODULE$ fut *max-await-duration*))
 
